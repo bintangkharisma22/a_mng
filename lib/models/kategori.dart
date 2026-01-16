997cdb32a3877f0034a771523e8ab94836944813
KbDevs12 <daniaku203@gmail.com>
2026-01-16 16:29:22 +0700

    merubah model kategori

--- lib/models/kategori.dart
+++ lib/models/kategori.dart
@@ -3,9 +3,24 @@ class Kategori {
   final String nama;
   final String kode;
 
-  Kategori({required this.id, required this.nama, required this.kode});
+  final DateTime createdAt;
+  final DateTime updatedAt;
+
+  Kategori({
+    required this.id,
+    required this.nama,
+    required this.kode,
+    required this.createdAt,
+    required this.updatedAt,
+  });
 
   factory Kategori.fromJson(Map<String, dynamic> json) {
-    return Kategori(id: json['id'], nama: json['nama'], kode: json['kode']);
+    return Kategori(
+      id: json['id'],
+      nama: json['nama'],
+      kode: json['kode'],
+      createdAt: DateTime.parse(json['created_at']),
+      updatedAt: DateTime.parse(json['updated_at']),
+    );
   }
 }
