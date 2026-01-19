9b6edfc3f6209f3a6d970e427b288723b804bddc
KbDevs12 <daniaku203@gmail.com>
2026-01-19 15:17:27 +0700

    fix divisi_service

--- lib/services/divisi_service.dart
+++ lib/services/divisi_service.dart
@@ -41,4 +41,36 @@ class DivisiService {
       throw Exception('Gagal menambah divisi');
     }
   }
+
+  static Future<Divisi> update(String id, Map<String, dynamic> body) async {
+    final token = await SessionManager.getToken();
+
+    final response = await http.put(
+      Uri.parse('$_baseUrl/api/lokasi/divisi/$id'),
+      headers: {
+        'Authorization': 'Bearer $token',
+        'Content-Type': 'application/json',
+      },
+      body: jsonEncode(body),
+    );
+
+    if (response.statusCode == 200) {
+      return Divisi.fromJson(jsonDecode(response.body));
+    } else {
+      throw Exception('Gagal update divisi');
+    }
+  }
+
+  static Future<void> delete(String id) async {
+    final token = await SessionManager.getToken();
+
+    final response = await http.delete(
+      Uri.parse('$_baseUrl/api/lokasi/divisi/$id'),
+      headers: {'Authorization': 'Bearer $token'},
+    );
+
+    if (response.statusCode != 200) {
+      throw Exception('Gagal hapus divisi');
+    }
+  }
 }
