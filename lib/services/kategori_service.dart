18a78857fa91e7ac95433b570e54056de09d9a65
KbDevs12 <daniaku203@gmail.com>
2026-01-19 14:13:38 +0700

    create kategori_service

---
+++ lib/services/kategori_service.dart
@@ -0,0 +1,44 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import '../core/config.dart';
+import '../core/session.dart';
+import '../models/kategori.dart';
+
+class KategoriService {
+  static const String _baseUrl = Config.baseUrl;
+
+  static Future<List<Kategori>> getKategori() async {
+    final token = await SessionManager.getToken();
+
+    final response = await http.get(
+      Uri.parse('$_baseUrl/api/lokasi/kategori'),
+      headers: {'Authorization': 'Bearer $token'},
+    );
+
+    if (response.statusCode == 200) {
+      final List data = jsonDecode(response.body);
+      return data.map((e) => Kategori.fromJson(e)).toList();
+    } else {
+      throw Exception('Gagal memuat kategori');
+    }
+  }
+
+  static Future<Kategori> create(Map<String, dynamic> body) async {
+    final token = await SessionManager.getToken();
+
+    final response = await http.post(
+      Uri.parse('$_baseUrl/api/lokasi/kategori'),
+      headers: {
+        'Authorization': 'Bearer $token',
+        'Content-Type': 'application/json',
+      },
+      body: jsonEncode(body),
+    );
+
+    if (response.statusCode == 201) {
+      return Kategori.fromJson(jsonDecode(response.body));
+    } else {
+      throw Exception('Gagal menambah kategori');
+    }
+  }
+}
