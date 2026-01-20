e47f294bffd1eec4cd6b0db1a0a72bb41980fbb1
KbDevs12 <daniaku203@gmail.com>
2026-01-20 13:40:12 +0700

    adding route

--- lib/core/routes.dart
+++ lib/core/routes.dart
@@ -3,5 +3,9 @@ class AppRoute {
   static const login = '/login';
   static const home = '/home';
   static const detailAset = '/aset/detail';
+
   static const ruangan = '/master/ruangan';
+  static const divisi = '/master/divisi';
+  static const kategori = '/master/kategori';
+  static const supplier = '/master/supplier';
 }
