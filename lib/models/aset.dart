bde760ef6b8f42914ca0c50c9afb9be1bec2068d
KbDevs12 <daniaku203@gmail.com>
2026-01-20 14:26:38 +0700

    fixing aset model

--- lib/models/aset.dart
+++ lib/models/aset.dart
@@ -7,27 +7,26 @@ class Aset {
   final String id;
   final String kodeAset;
   final String? nomorSeri;
-  final String? status;
-
-  final Kategori? kategori;
-  final Ruangan? ruangan;
-  final Divisi? divisi;
-  final KondisiAset? kondisi;
-
-  final String? gambar;
+  final String status;
   final String? qrCode;
+  final String? gambar;
+
+  final Kategori kategori;
+  final Ruangan ruangan;
+  final Divisi divisi;
+  final KondisiAset kondisi;
 
   Aset({
     required this.id,
     required this.kodeAset,
+    required this.status,
+    required this.kategori,
+    required this.ruangan,
+    required this.divisi,
+    required this.kondisi,
     this.nomorSeri,
-    this.status,
-    this.kategori,
-    this.ruangan,
-    this.divisi,
-    this.kondisi,
-    this.gambar,
     this.qrCode,
+    this.gambar,
   });
 
   factory Aset.fromJson(Map<String, dynamic> json) {
@@ -36,18 +35,12 @@ class Aset {
       kodeAset: json['kode_aset'],
       nomorSeri: json['nomor_seri'],
       status: json['status'],
-      gambar: json['gambar'],
       qrCode: json['qr_code'],
-      kategori: json['kategori'] != null
-          ? Kategori.fromJson(json['kategori'])
-          : null,
-      ruangan: json['ruangan'] != null
-          ? Ruangan.fromJson(json['ruangan'])
-          : null,
-      divisi: json['divisi'] != null ? Divisi.fromJson(json['divisi']) : null,
-      kondisi: json['kondisi'] != null
-          ? KondisiAset.fromJson(json['kondisi'])
-          : null,
+      gambar: json['gambar'],
+      kategori: Kategori.fromJson(json['kategori']),
+      ruangan: Ruangan.fromJson(json['ruangan']),
+      divisi: Divisi.fromJson(json['divisi']),
+      kondisi: KondisiAset.fromJson(json['kondisi']),
     );
   }
 }
